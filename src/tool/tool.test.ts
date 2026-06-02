14efdd604251248b4dd8799f85bfcdd37555aaee
tao <tao@none.com>
2026-06-02 18:45:44 +0800

    number、float、string

--- src/tool/tool.test.ts
+++ src/tool/tool.test.ts
@@ -1,8 +1,8 @@
-import { describe, expect, test } from "vitest";
-import { increment } from "./index";
+import { describe, expect, test } from 'vitest'
+import { increment } from './index'
 
-describe("test increment", () => {
-  test("increment 1 to be 2", ()=> {
+describe('test increment', () => {
+  test('increment 1 to be 2', () => {
     expect(increment(1)).toBe(2)
   })
 })
@@ -22,4 +22,29 @@ test('zero', () => {
   expect(z).toBeFalsy()
   expect(z).toBeDefined()
   expect(z).not.toBeNull()
+})
+
+test('number comparisons', () => {
+  const value = 2 + 2
+  expect(value).toBeGreaterThan(3)
+  expect(value).toBeGreaterThanOrEqual(3.5)
+  expect(value).toBeLessThan(5)
+
+  expect(value).toBe(4)
+  expect(value).toEqual(4)
+})
+
+test('adding float point numbers', () => {
+  const value = 0.1 + 0.2
+  expect(value).not.toBe(0.3)
+  expect(value).toBeCloseTo(0.3)
+})
+
+describe('match string', () => {
+  test('there is no I in team', () => {
+    expect('team').not.toMatch(/I/)
+  })
+  test('version string matches semver format', () => {
+    expect('vitest@4.2.0').toMatch(/vitest@\d+\.\d+\.\d+/)
+  })
 })
